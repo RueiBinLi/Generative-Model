04e97b5ee9d224ea65c98d0f723dbd9e8728c719
Robby Li <robby@DESKTOP-OK4CGHD.localdomain>
2025-09-21 21:55:41 +0800

    modify cosine scheduler

--- Lab1-DDPM/image_diffusion_todo/scheduler.py
+++ Lab1-DDPM/image_diffusion_todo/scheduler.py
@@ -41,11 +41,11 @@ class BaseScheduler(nn.Module):
             #       beta_t = 1 - alphā_t / alphā_{t-1}
             # 3. Return betas as a tensor of shape [num_train_timesteps].
             s = 0.008
-            t = torch.linspace(0, num_train_timesteps, steps=num_train_timesteps+1)
+            t = torch.linspace(0, 1, steps=num_train_timesteps+1)
             f_t = torch.cos(((t / num_train_timesteps + s) / (1 + s)) * (torch.pi / 2)) ** 2
             alpha_cumprod_t = f_t / f_t[0]
             betas = 1 - alpha_cumprod_t[1:] / alpha_cumprod_t[:-1] # [1:T] / [1:T-1] Noted: alpht_cumprod_t[0] = 1
-            betas = torch.clip(betas, 0.0, 0.999)        
+            betas = torch.clip(betas, 0.0001, 0.9999)        
         else:
             raise NotImplementedError(f"{mode} is not implemented.")
 
